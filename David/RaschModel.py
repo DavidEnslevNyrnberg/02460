925e9a9ee8e27f663dfacf8067edc2f18a096d45
DavidEnslevNyrnberg <andennygi@hotmail.com>
2018-05-20 11:25:00 +0200

    teresa update with plot update

--- David/RaschModel.py
+++ David/RaschModel.py
@@ -7,6 +7,7 @@ from scipy.optimize import minimize
 
 # plot enabler
 doPLOT = 1
+figSaver = 0
 
 # Loading of real data
 Dir = os.getcwd()
@@ -33,8 +34,8 @@ dfRaschD = dfData[headQ[0:-1]]
 # fix random seed for comparing results
 np.random.seed(10)
 # set number of test items and number of students
-iTest = 100
-nStudent = 500
+iTest = 20
+nStudent = 100
 
 # Simulate student abilities
 sdStu = 0.8
@@ -61,6 +62,8 @@ if doPLOT == 1:
     ax2.set_title("$i=20$, Test difficulty distribution")
     ountTest, binsTest, ignoredTest = plt.hist(deltaTrue, bins=20, normed=True)
     plt.plot(binsTest, 1/(sdTest*np.sqrt(2*np.pi))*np.exp(-(binsTest-meanTest)**2/(2*sdTest**2)), linewidth=2, color='r')
+    if figSaver == 1:
+        plt.savefig('plots/wDistribution')
     plt.show()
 
 # print(betaTrue); print(deltaTrue)
@@ -134,20 +137,26 @@ beta_w = w_new[:nStudent]
 
 if doPLOT==1:
     # plots for real weights against estimated weights
-    plt.figure(1, figsize=[6,6])
-    plt.tight_layout()
-    plt.subplot(2,1,1)
-    plt.plot(delta_w, deltaTrue, 'bo', deltaTrue, deltaTrue, 'g-')
-    plt.title(r'True $\delta_i$ versus Estimated $\delta_i$')
+    plt.figure(1, figsize=[6, 6])
+    # plt.tight_layout()
+    # plt.subplot(2,1,1)
+    plt.plot(delta_w, deltaTrue, 'bx', deltaTrue, deltaTrue, 'g-')
+    plt.title(r'[i=%d] True $\delta_i$ versus Estimated $\delta_i$' % iTest)
     plt.xlabel(r'True $\delta_i$')
     plt.ylabel(r'Estimated $\delta_i$')
-    plt.legend(['estVtrue','trueLine'], loc=2)
-    plt.subplot(2,1,2)
-    plt.plot(beta_w, betaTrue, 'bo', betaTrue, betaTrue, 'g-')
-    plt.title(r'True $\beta_n$ versus Estimated $\beta_n$')
+    plt.legend(['est_vs_true', 'trueLine'], loc=2)
+    if figSaver == 1:
+        plt.savefig('plots/EstimatedDeltaNonPriv')
+    plt.show()
+    # plt.subplot(2,1,2)
+    plt.figure(2, figsize=[6, 6])
+    plt.plot(beta_w, betaTrue, 'bx', betaTrue, betaTrue, 'g-')
+    plt.title(r'[n=%d] True $\beta_n$ versus Estimated $\beta_n$' % nStudent)
     plt.xlabel(r'True $\beta_n$')
     plt.ylabel(r'Estimated $\beta_n$')
-    plt.legend(['estVtrue','trueLine'], loc=2)
+    plt.legend(['est_vs_true', r'true $\beta_n$'], loc=2)
+    if figSaver == 1:
+        plt.savefig('plots/EstimatedBetaNonPriv')
     plt.show()
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -159,13 +168,17 @@ for n in range(nStudent):
 
 if doPLOT==1:
     # plots for real weights against estimated weights
-    plt.figure(2, figsize=[6,6])
+    plt.figure(4, figsize=[10, 10])
     plt.tight_layout()
-    plt.plot(pRasch[:100], pRaschEST[:100], 'bx', pRasch[:100], pRasch[:100], 'g-')
-    plt.title(r'Estimated $p(X_{ni}=1)$ vs True $p(X_{ni}=1)$')
-    plt.xlabel(r'True $p(X_{ni}=1)$')
-    plt.ylabel(r'Estimated $p(X_{ni}=1)$')
-    plt.legend(['Estimated','True'], loc=2)
+    plt.plot(pRasch[:100], pRaschEST[:100], 'bx', markersize=8)
+    plt.plot(pRasch[1:], pRasch[1:], 'g-', linewidth=7)
+    plt.title(r'[N=%d]  $p(X_{ni}=1)_{est}$ vs $p(X_{ni}=1)_{true}$' % nStudent, fontdict={'fontsize': 30})
+    plt.xlabel(r'$p(X_{ni}=1)_{true}$', fontdict={'fontsize': 30})
+    plt.ylabel(r'$p(X_{ni}=1)_{est}$', fontdict={'fontsize': 30})
+    plt.legend(['Estimated', 'True'], loc=2, fontsize=20)
+    plt.tick_params(axis='both', labelsize=20)
+    if figSaver == 1:
+        plt.savefig('plots/EstimatedVsTrueNonPriv')
     plt.show()
 
 # plt.scatter(pRasch[:100], pRaschEST[:100])
